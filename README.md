8826ceedb5592426649f7a513421faf3f94c1b93
jfc4050 <jfc4050@gmail.com>
2019-07-29 19:28:38 -0700

    updated setup script

--- README.md
+++ README.md
@@ -1,25 +1,37 @@
 # dotfiles
 
-install zsh + oh-my-zsh, install conda, symlink dotfiles
-
 ```bash
 git clone --recursive https://github.com/jfc4050/dotfiles.git
 cd dotfiles
+```
+
+## dotfiles and zsh+vim themes
+First install:
+* zsh
+* oh-my-zsh
+* tmux
+* anaconda
+
+Then, to symlink dotfiles and themes:
+
+```bash
 chmod +x setup.sh
 ./setup.sh
 ```
 
-DE setup (linux only)
-
-requires:
-
+## DE setup (Linux only)
+First install:
 * bspwm
 * sxhkd
 * compton (tryone144 fork)
 * ulauncher
 * terminator
 
+Then to symlink config files:
+
 ```bash
 chmod +x de_setup.sh
 ./de_setup.sh
 ```
+
+note that `bspwmrc` uses the output of `cat /var/lib/dbus/machine-id` to determine if i am at work or home and picks monitor/keyboard options accordingly. You will need to modify this for your own setup.
--- setup.sh
+++ setup.sh
@@ -1,44 +1,17 @@
 #!/bin/bash
 
-### zsh
-command -v zsh &> /dev/null || { echo "installing zsh"; sudo apt install -y zsh; }
-sudo chsh $USER -s $(which zsh)
+set -e
 
-### oh-my-zsh
-if [ ! -d "$HOME/.oh-my-zsh" ]
-then
-    echo "installing oh-my-zsh"
-    sh -c "$(wget -qO- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-fi
-
-### conda
-if [ ! $(command -v conda) ]
-then
-    echo "installing conda"
-    case "$OSTYPE" in
-        "linux*") OS="Linux";;
-        "darwin") OS="MacOSX";;
-        *) OS="Linux";;
-    esac
-    CONTREPO=https://repo.continuum.io/archive
-    SCRIPTNAME=$(wget -qO- $CONTREPO | grep -o "Anaconda[0-9]-[0-9]\.[0-9]\.[0-9]-$OS-x86_64.sh" | sort -r | head -n 1)
-    wget $CONTREPO/$SCRIPTNAME
-    zsh $SCRIPTNAME
-fi
-
-### place dotfiles
-for Dotfile in .zshrc .vimrc .tmux.conf .condarc
-do
-    mv $HOME/$Dotfile $HOME/$Dotfile.backup
-    ln -s $(pwd)/$Dotfile $HOME/$Dotfile
-done
-
-### zsh theme
 ZSH_THEMEDIR="$HOME/.oh-my-zsh/custom/themes"
-cp -rs $(pwd)/spaceship-prompt $ZSH_THEMEDIR &> /dev/null
-ln -s "$ZSH_THEMEDIR/spaceship-prompt/spaceship.zsh-theme" "$ZSH_THEMEDIR/spaceship.zsh-theme"
-
-### vim theme
 VIM_THEMEDIR="$HOME/.vim/colors"
+
+ln -sbr .zshrc .vimrc .tmux.conf .condarc $HOME/
+
+# zsh theme
+mkdir -p $ZSH_THEMEDIR
+cp -nrs $(pwd)/spaceship-prompt $ZSH_THEMEDIR
+ln -sb $ZSH_THEMEDIR/spaceship-prompt/spaceship.zsh-theme $ZSH_THEMEDIR/
+
+# vim theme
 mkdir -p $VIM_THEMEDIR
-ln -s $(pwd)/tomorrow-theme/vim/colors/Tomorrow-Night.vim $VIM_THEMEDIR/Tomorrow-Night.vim
+ln -sb $(pwd)/tomorrow-theme/vim/colors/Tomorrow-Night.vim $VIM_THEMEDIR/Tomorrow-Night.vim
