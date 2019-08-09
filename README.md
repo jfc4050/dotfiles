bfa0125eacc841f91bf093c86074d7383411cfe3
justin <justin.chiu@apollovideo.com>
2019-08-09 00:12:37 -0700

    all setup done using single script

--- README.md
+++ README.md
@@ -1,40 +1,28 @@
 # dotfiles
 
-```bash
-git clone --recursive https://github.com/jfc4050/dotfiles.git
-cd dotfiles
-```
-
-## dotfiles and zsh+vim themes
 First install:
 * zsh
 * oh-my-zsh
 * tmux
 * anaconda
+* vscode
+* bspwm
+* sxhkd
+* compton ([tryone144 fork](https://github.com/tryone144/compton))
+* terminator
+* ulauncher
 
-Then, to symlink dotfiles and themes:
+Then, to symlink dotfiles and setup themes/plugins:
 
 ```bash
+git clone --recursive https://github.com/jfc4050/dotfiles.git
+cd dotfiles
 chmod +x setup.sh
 ./setup.sh
 ```
 
-## DE setup (Linux only)
-First install:
-* bspwm
-* sxhkd
-* compton (tryone144 fork)
-* ulauncher
-* terminator
-* vscode
-
-Then to symlink config files:
-
-```bash
-chmod +x de_setup.sh
-./de_setup.sh
-```
+after that any config updates can be applied simply by doing a `git pull --rebase`.
 
 note that `bspwmrc` uses the output of `cat /var/lib/dbus/machine-id` to determine if i am at work or home and picks monitor/keyboard options accordingly. You will need to modify this for your own setup.
 
-GTK theme based on https://github.com/vinceliuice/Mojave-gtk-theme
+GTK theme taken from https://github.com/vinceliuice/Mojave-gtk-theme
--- setup.sh
+++ setup.sh
@@ -2,21 +2,32 @@
 
 set -e
 
+for dfile_srcpath in $(find HOME -type f)
+do
+    if [[ $(echo $dfile_srcpath | awk -F'.' '{print $NF}') == "sh" ]]
+    then
+        echo "giving execute permissions to $(basename $dfile_srcpath)"
+        chmod +x $dfile_srcpath
+    fi
+
+    dfile_dstpath=$(echo $dfile_srcpath | sed "s|HOME|$HOME|")
+    mkdir -p $(dirname $dfile_dstpath)
+    ln -sb $(pwd)/$dfile_srcpath $dfile_dstpath
+done
+
 ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
 ZSH_PLUGINS="$ZSH_CUSTOM/plugins"
 ZSH_THEMES="$ZSH_CUSTOM/themes"
 VIM_THEMES="$HOME/.vim/colors"
 
-ln -sbr .zshrc .vimrc .tmux.conf .condarc $HOME/
-
 # zsh theme
-mkdir -p $ZSH_THEMES
-cp -nrs $(pwd)/spaceship-prompt $ZSH_THEMES
+mkdir -p $ZSH_THEMES/
+cp -nrs $(pwd)/spaceship-prompt $ZSH_THEMES/
 ln -sb $ZSH_THEMES/spaceship-prompt/spaceship.zsh-theme $ZSH_THEMES/
 
-# autosuggestions
-cp -nrs $(pwd)/zsh-autosuggestions $ZSH_PLUGINS
-
 # vim theme
-mkdir -p $VIM_THEMES
-ln -sb $(pwd)/tomorrow-theme/vim/colors/Tomorrow-Night.vim $VIM_THEMES/Tomorrow-Night.vim
+mkdir -p $VIM_THEMES/
+ln -sb $(pwd)/tomorrow-theme/vim/colors/Tomorrow-Night.vim $VIM_THEMES/
+
+# autosuggestions
+cp -nrs $(pwd)/zsh-autosuggestions $ZSH_PLUGINS/
