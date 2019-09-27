f96d83d34a219f17327a6679fbb6ded5acced6eb
Justin Chiu <jfc4050@gmail.com>
2019-09-26 21:39:19 -0700

    setup script cleanup
        - more verbosity
        - using braces for expansion

--- setup.sh
+++ setup.sh
@@ -4,30 +4,24 @@ set -e
 
 for dfile_srcpath in $(find HOME -type f)
 do
-    if [[ $(echo $dfile_srcpath | awk -F'.' '{print $NF}') == "sh" ]]
+    if [[ $(echo ${dfile_srcpath} | awk -F'.' '{print $NF}') == "sh" ]]
     then
-        echo "giving execute permissions to $(basename $dfile_srcpath)"
-        chmod +x $dfile_srcpath
+        chmod -v +x ${dfile_srcpath}
     fi
 
-    dfile_dstpath=$(echo $dfile_srcpath | sed "s|HOME|$HOME|")
-    mkdir -p $(dirname $dfile_dstpath)
-    ln -sb $(pwd)/$dfile_srcpath $dfile_dstpath
+    dfile_dstpath=$(echo ${dfile_srcpath} | sed "s|HOME|${HOME}|")
+    mkdir -pv $(dirname ${dfile_dstpath})
+    ln -bsv $(pwd)/${dfile_srcpath} ${dfile_dstpath}
 done
 
-ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
-ZSH_PLUGINS="$ZSH_CUSTOM/plugins"
-ZSH_THEMES="$ZSH_CUSTOM/themes"
-VIM_THEMES="$HOME/.vim/colors"
+ZSH_CUSTOM="${HOME}/.oh-my-zsh/custom"
+ZSH_PLUGINS="${ZSH_CUSTOM}/plugins"
+ZSH_THEMES="${ZSH_CUSTOM}/themes"
+VIM_THEMES="${HOME}/.vim/colors"
 
-# zsh theme
-mkdir -p $ZSH_THEMES/
-cp -nrs $(pwd)/spaceship-prompt $ZSH_THEMES/
-ln -sb $ZSH_THEMES/spaceship-prompt/spaceship.zsh-theme $ZSH_THEMES/
+mkdir -pv ${ZSH_PLUGINS} ${ZSH_THEMES} ${VIM_THEMES}
 
-# vim theme
-mkdir -p $VIM_THEMES/
-ln -sb $(pwd)/tomorrow-theme/vim/colors/Tomorrow-Night.vim $VIM_THEMES/
-
-# autosuggestions
-cp -nrs $(pwd)/zsh-autosuggestions $ZSH_PLUGINS/
+cp -nrsv $(pwd)/spaceship-prompt ${ZSH_THEMES}
+ln -bsv ${ZSH_THEMES}/spaceship-prompt/spaceship.zsh-theme ${ZSH_THEMES}
+ln -bsv $(pwd)/tomorrow-theme/vim/colors/Tomorrow-Night.vim ${VIM_THEMES}
+cp -nrsv $(pwd)/zsh-autosuggestions ${ZSH_PLUGINS}
