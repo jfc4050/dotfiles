8381dd23cfd95f5bbb6375eaaf50d46a1c5cf498
Justin Chiu <jfc4050@gmail.com>
2019-09-26 21:08:31 -0700

    added .zshrc for osx_setup
        - zsh theme
        - zsh autocomplete

--- osx_setup.sh
+++ osx_setup.sh
@@ -2,6 +2,17 @@
 
 set -e
 
+ZSH_CUSTOM="${HOME}/.oh-my-zsh/custom"
+ZSH_PLUGINS="${ZSH_CUSTOM}/plugins"
+ZSH_THEMES="${ZSH_CUSTOM}/themes"
+
+mkdir -p ${ZSH_PLUGINS} ${ZSH_THEMES}
+
+ln -fsv $(pwd)/HOME/.zshrc ${HOME}
+ln -fsv $(pwd)/zsh-autosuggestions ${ZSH_PLUGINS}/zsh-autosuggestions
+ln -fsv $(pwd)/spaceship-prompt ${ZSH_THEMES}/spaceship-prompt
+ln -fsv ${ZSH_THEMES}/spaceship-prompt/spaceship.zsh-theme ${ZSH_THEMES}
+
 for vsc_cfgfile in settings keybindings
 do
     vsc_srcpath="$(pwd)/HOME/.config/Code/User/${vsc_cfgfile}.json"
